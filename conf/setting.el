3eaaa876e84688be3558679f3f3e2fef47596919
Emi Keisuke <emi@swlab.cs.okayama-u.ac.jp>
2017-10-06 08:29:22 +0900

    Add revert-buffer-no-confirm command

--- conf/setting.el
+++ conf/setting.el
@@ -52,6 +52,20 @@
 (global-set-key (kbd "M-n") (lambda () (interactive) (scroll-up 1)))
 (global-set-key (kbd "M-p") (lambda () (interactive) (scroll-down 1)))
 
+;; バッファを再読込
+(defun revert-buffer-no-confirm (&optional force-reverting)
+  "Interactive call to revert-buffer. Ignoring the auto-save
+ file and not requesting for confirmation. When the current buffer
+ is modified, the command refuses to revert it, unless you specify
+ the optional argument: force-reverting to true."
+  (interactive "P")
+  (message "reverting buffer")
+  (if (or force-reverting (not (buffer-modified-p)))
+      (revert-buffer :ignore-auto :noconfirm)
+    (error "The buffer has been modified")))
+
+(global-set-key (kbd "M-r") 'revert-buffer-no-confirm)
+
 ;;; Macのみに有効な設定
 (when (eq system-type 'darwin)
   ;; 右のCommandキーをMetaキーとして使用
